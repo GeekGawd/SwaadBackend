9dfd5632be8e10c0133daed050b8e8ebe4160a9b
Suyash Singh <suyashsingh.stem@gmail.com>
2021-11-02 19:17:07 +0530

    Updated SMTP settings

--- app/settings.py
+++ app/settings.py
@@ -128,8 +128,9 @@ django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'core.User'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS=True
